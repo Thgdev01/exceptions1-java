ac8fbd8a450f1591e744a470d4950354cc84baf3
Thiag0 <thiagodealmeidamelo@gmail.com>
2024-08-05 19:27:50 -0300

    Sol ruim 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,19 +20,33 @@ public class Program {
 		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
 		
-		Reservation reservation = new Reservation(number, checkIn, checkOut);
-		System.out.println("Reservation: " + reservation);
+		if (!checkOut.after(checkIn)) {
+			System.out.println("Error: check-out date must be after check-in date");
+		}
 		
-		System.out.println();
-		System.out.println("Enter data to update the reservation:");
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		checkOut = sdf.parse(sc.next());
+		else {
 		
-		reservation.updateDates(checkIn, checkOut);
-		System.out.println("Reservation: " + reservation);
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
 		
+		
+			System.out.println();
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			Date now = new Date();
+			if (checkOut.before(now) || checkIn.before(now)) {
+				System.out.println("Error: Reservation dates for update are invalid!");
+			}
+			else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println("Reservation: " + reservation);
+			}
+		
+		}
 		
 		sc.close();
 
